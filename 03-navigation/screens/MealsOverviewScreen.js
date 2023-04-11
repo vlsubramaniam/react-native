deb3faaa5028e777300fc9cef7cab9a25c2c584d
vlsubramaniam <subramaniam.vls@gmail.com>
2023-04-11 17:38:34 +0530

    11-APR-2023

--- 03-navigation/screens/MealsOverviewScreen.js
+++ 03-navigation/screens/MealsOverviewScreen.js
@@ -1,11 +1,33 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const { categoryId } = route.params;
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
+  function renderMealItem(itemData) {
+    const { item } = itemData;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
