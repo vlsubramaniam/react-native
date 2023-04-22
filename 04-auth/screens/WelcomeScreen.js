65d3bd62813c0a5d299deb471080712037bd8de4
vlsubramaniam <subramaniam.vls@gmail.com>
2023-04-22 09:21:47 +0530

    22-APR-2023

--- 04-auth/screens/WelcomeScreen.js
+++ 04-auth/screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { AuthContext } from '../store/AuthContext';
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-e713e-default-rtdb.firebaseio.com/message.json?auth=' +
+          authCtx.token
+      )
+      .then((response) => setMessage(response.data));
+  }, [authCtx.token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
